378e5737bd62d12d02bd782be064ae81cdaa599b
Mugenzi-Edgar <mugenziedgar65@gmail.com>
2026-03-14 20:07:54 +0200

    Database 3 configuration

--- config/db.php
+++ config/db.php
@@ -11,5 +11,4 @@ $conn = mysqli_connect($host, $user, $password, $database, $port);
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
 ?>
