d3dc01c761d3fa951e25727d817855a10df09a41
Mugenzi-Edgar <mugenziedgar65@gmail.com>
2026-03-14 20:15:45 +0200

    Database 3 configuration

--- coding-academy/config/db.php
+++ coding-academy/config/db.php
@@ -1,6 +1,14 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "nga_deployment");
-if ($conn->connect_error) {
-    die("Database connection failed");
+// 1. Host MUST be the AWS RDS Endpoint
+$host = "web.c4r4q0g4u6vz.us-east-1.rds.amazonaws.com"; 
+// 2. User MUST be your master username (or a specifically created user)
+$user = "admin"; 
+// 3. Password MUST be your master password for the admin user
+$password = "Felix12345!"; 
+$database = "nga_db";
+$port = 3306;
+$conn = mysqli_connect($host, $user, $password, $database, $port);
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
 }
 ?>
