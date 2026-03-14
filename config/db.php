7a229d0c35db36fd58fa744baa1de8f75de44e22
Mugenzi-Edgar <mugenziedgar65@gmail.com>
2026-03-14 17:52:28 +0200

    Database configuration

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "nga_deployment");
+$conn = new mysqli("web.c4r4q0g4u6vz.us-east-1.rds.amazonaws.com", "admin", "StrongPassword123", "web");
 if ($conn->connect_error) {
     die("Database connection failed");
 }
