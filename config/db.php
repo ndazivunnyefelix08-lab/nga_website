5548a4c6de40c36cbb71a7c6905b06faad1e4643
Mugenzi-Edgar <mugenziedgar65@gmail.com>
2026-03-14 19:27:43 +0200

    Database 3 configuration

--- config/db.php
+++ config/db.php
@@ -1,21 +1,15 @@
 <?php
 // 1. Host MUST be the AWS RDS Endpoint
 $host = "web.c4r4q0g4u6vz.us-east-1.rds.amazonaws.com"; 
-
 // 2. User MUST be your master username (or a specifically created user)
 $user = "admin"; 
-
 // 3. Password MUST be your master password for the admin user
-$password = "nga12345"; 
-
+$password = "Felix12345!"; 
 $database = "mysql";
 $port = 3306;
-
 $conn = mysqli_connect($host, $user, $password, $database, $port);
-
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
-
 echo "Connected successfully";
 ?>
