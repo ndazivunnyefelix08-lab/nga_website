0a54a3b33d64fb071e9909e96b772c1b2610252d
Mugenzi-Edgar <mugenziedgar65@gmail.com>
2026-03-14 18:38:33 +0200

    Database 2 configuration

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 // Note: Changed the 4th parameter from "web" to "mysql"
-$conn = new mysqli("web.c4r4q0g4u6vz.us-east-1.rds.amazonaws.com", "admin", "StrongPassword123", "mysql");
+$conn = new mysqli("web.c4r4q0g4u6vz.us-east-1.rds.amazonaws.com", "web", "Kigali@@12345!!", "mysql");
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
